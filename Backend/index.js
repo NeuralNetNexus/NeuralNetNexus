067db594a0dc3cf5b27447a3b9f7fb50598fa5ea
carla <carlasofiamendes@outlook.com>
2023-06-01 20:24:16 +0100

    MongoDB -  user model

--- Backend/index.js
+++ Backend/index.js
@@ -5,8 +5,16 @@ const multer = require('multer');
 const unzipper = require('unzipper');
 const archiver = require('archiver');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('Failed to connect to MongoDB', err));
+  
 app.use(cors());
 const upload = multer({ dest: 'uploads/' });
 
---
+++ Backend/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['client', 'admin'], required: true },
+    name: { type: String, required: false, unique: false },    
+}, { timestamps: { createdAt: true, updatedAt: false } });
+
+module.exports = mongoose.model('User', userSchema);
