a9d9542b00b23483294bb6298dcd3e21ca7d3012
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-06-02 13:18:58 +0100

    train_suppervisor job creating

--- train_suppervisor/main.py
+++ train_suppervisor/main.py
@@ -5,11 +5,11 @@ import yaml
 
 from kubernetes import client, config
 
-def create_job_object(job_name):
+def create_job_object(job_name, image_name, completions=None, parallelism=None):
     # Define the job's container
     container = client.V1Container(
-        name="split",
-        image="rafaelxokito/neuralnetnexussplit:latest",
+        name=job_name,
+        image=image_name,
     )
     # Define the job's template
     template = client.V1PodTemplateSpec(
@@ -17,10 +17,18 @@ def create_job_object(job_name):
         spec=client.V1PodSpec(restart_policy="Never", containers=[container]),
     )
     # Define the job's spec
-    spec = client.V1JobSpec(
-        template=template,
-        backoff_limit=4,
-    )
+    if completions and parallelism:
+        spec = client.V1JobSpec(
+            completions=completions,
+            parallelism=parallelism,
+            template=template,
+            backoff_limit=4,
+        )
+    else:
+        spec = client.V1JobSpec(
+            template=template,
+            backoff_limit=4,
+        )
     # Instantiate the job object
     job = client.V1Job(
         api_version="batch/v1",
@@ -31,12 +39,11 @@ def create_job_object(job_name):
 
     return job
 
-def create_job(api_instance, job, job_name):
+def create_job(api_instance, job):
     api_response = api_instance.create_namespaced_job(
         body=job,
         namespace="default")
     print("Job created. status='%s'" % str(api_response.status))
-    get_job_status(api_instance, job_name)
 
 def get_job_status(api_instance, job_name):
     job_completed = False
@@ -56,15 +63,29 @@ def main():
     # default location.
     config.load_kube_config()
     batch_v1 = client.BatchV1Api()
-    # Create a job object with client-python API. The job we
-    # created is same as the `pi-job.yaml` in the /examples folder.
-    job_name = "split-job"
     
-    job = create_job_object(job_name)
+    # {1} -> é o ID do projeto
 
-    create_job(batch_v1, job, job_name)
+    # Split Job
+    split_job_name = f"split-job-{1}"
+    split_job = create_job_object(split_job_name, "rafaelxokito/neuralnetnexussplit:latest")
+    create_job(batch_v1, split_job)
+    get_job_status(batch_v1, split_job_name)
 
-    get_job_status(batch_v1, job_name)
+    # TODO -> Ir buscar o número de batches do 'split' job e definir o número de 
+    # completions e parallelism para o número de batches
+
+    # Training Jobs
+    train_job_name = f"train-job-{1}"
+    train_job = create_job_object(train_job_name, "rafaelxokito/neuralnetnexustrain:latest", completions=5, parallelism=5)
+    create_job(batch_v1, train_job)
+    get_job_status(batch_v1, train_job_name)
+
+    # Aggregator Job
+    aggregator_job_name = f"aggregator-job-{1}"
+    aggregator_job = create_job_object(aggregator_job_name, "rafaelxokito/neuralnetnexusagregator:latest")
+    create_job(batch_v1, aggregator_job)
+    get_job_status(batch_v1, aggregator_job_name)
 
 if __name__ == '__main__':
     main()
