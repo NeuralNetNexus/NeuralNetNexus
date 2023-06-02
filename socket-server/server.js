f6072c91afdef1430d562286b069b500443c3faf
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-06-02 11:47:30 +0100

    real time messaging service

---
+++ socket-server/server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const { Server } = require('socket.io');
+
+const PORT = process.env.PORT || 3002;
+ 
+const httpServer = http.createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id);
+
+  socket.on('joinProject', (projectId) => {
+    socket.join(projectId);
+  });
+
+  socket.on('project', (projectId, message) => {
+    io.to(projectId).emit('receiveMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Socket.IO server running on port ${PORT}`);
+}); 
