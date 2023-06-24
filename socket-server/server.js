1e17fae58dd474b84ba3cf54e4f53308836d7bf4
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-06-24 21:45:00 +0100

    Update server.js

--- socket-server/server.js
+++ socket-server/server.js
@@ -20,21 +20,25 @@ io.on('connection', (socket) => {
 
   // Live status of the project from a project supervisor
   socket.on('projectStatus', (projectId, status) => {
+    console.log('projectStatus', projectId, status);
     io.to(projectId).emit('receiveProjectStatus', status);
   });
 
   // A number related to the project from a related client
   socket.on('splitNumber', (projectId, number) => {
+    console.log('splitNumber', projectId, number);
     io.to(projectId).emit('receiveSplitNumber', number);
   });
 
   // Several metrics related to a process from a related client
   socket.on('trainingMetrics', (projectId, metrics) => {
+    console.log('trainingMetrics', projectId, metrics);
     io.to(projectId).emit('receiveTrainingMetrics', metrics);
   });
 
   // Several metrics related to a final process from a related client
   socket.on('aggregatorMetrics', (projectId, metrics) => {
+    console.log('aggregatorMetrics', projectId, metrics);
     io.to(projectId).emit('receiveAggregatorMetrics', metrics);
   });
 
