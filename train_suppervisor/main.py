cc834a02d2d10667dedf7c437dc6b401425452bf
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-06-25 00:35:55 +0100

    Update main.py

--- train_suppervisor/main.py
+++ train_suppervisor/main.py
@@ -108,7 +108,7 @@ def main():
     
     # {1} -> é o ID do projeto
 
-    sio.connect('ws://socket-server')
+    sio.connect('ws://socket-service')
 
     # =================  Split Job  ================= #
 
